16c0aed0ba4ce35ab8da3a32a00d754339267fed
Fra <francesco.goinavi@gmail.com>
2022-04-11 18:37:20 +0200

    Move Wire class again

--- extension.js
+++ extension.js
@@ -31,6 +31,10 @@ function init() {
   return new Extension();
 }
 
+function wire(target, signal, handler) {
+  return new Wire(target, signal, handler);
+}
+
 function _activationFlagsToString(flags) {
   let string = '';
   for (const name in ActivationFlags) {
@@ -84,39 +88,6 @@ function _relativeEdge(boxA, boxB) {
   }
 
   return Edge.NONE;
-}
-
-function wire(target, signal, handler) {
-  return new Wire(target, signal, handler);
-}
-
-class Wire {
-  constructor(target, signal, handler) {
-    this._target = target;
-    this._signal = signal;
-    this._handler = handler;
-    this._handlerId = 0;
-  }
-
-  connect() {
-    if (!this._handlerId && this._target) {
-      this._handlerId = this._target.connect(this._signal, this._handler);
-    }
-    return this;
-  }
-
-  disconnect() {
-    if (this._handlerId) {
-      this._target.disconnect(this._handlerId);
-      this._handlerId = 0;
-    }
-  }
-
-  setTarget(target) {
-    this.disconnect();
-    this._target = target;
-    return this;
-  }
 }
 
 class Extension {
@@ -1047,5 +1018,34 @@ class UnredirectHelper {
       Meta.enable_unredirect_for_display(global.display);
     }
     this._disabled = value;
+  }
+}
+
+class Wire {
+  constructor(target, signal, handler) {
+    this._target = target;
+    this._signal = signal;
+    this._handler = handler;
+    this._handlerId = 0;
+  }
+
+  connect() {
+    if (!this._handlerId && this._target) {
+      this._handlerId = this._target.connect(this._signal, this._handler);
+    }
+    return this;
+  }
+
+  disconnect() {
+    if (this._handlerId) {
+      this._target.disconnect(this._handlerId);
+      this._handlerId = 0;
+    }
+  }
+
+  setTarget(target) {
+    this.disconnect();
+    this._target = target;
+    return this;
   }
 }
